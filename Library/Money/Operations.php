3085d2f219d31de0d5a1fec1c24fc22278d645d9
Zdeněk Kolovrat <graetar@gmail.com>
2025-02-17 02:22:25 +0100

    Update Operations.php

--- Library/Money/Operations.php
+++ Library/Money/Operations.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace Library\Money;
+declare(strict_types=1);
 
+namespace Library\Money;
 
 /**
  * Class Operations
@@ -10,17 +11,13 @@ namespace Library\Money;
 final class Operations implements IOperations
 {
 
-    /** @var IExchangeRate */
-    protected $exchangeRate;
+    protected IExchangeRate $exchangeRate;
 
     public function __construct()
     {
         $this->exchangeRate = new ExchangeRate();
     }
-
-    /**
-     * @inheritDoc
-     */
+    
     public function addAmount(
         float $amount1,
         string $currency1,
@@ -28,16 +25,14 @@ final class Operations implements IOperations
         string $currency2,
         string $outputCurrency,
         ?int $precision
-    ): float {
+    ): float
+    {
         $convertedAmount1 = $this->exchangeRate->convert($amount1, $currency1, $outputCurrency, $precision);
-        $convertedAmount2 = $this->exchangeRate->convert($amount1, $currency1, $outputCurrency, $precision);
+        $convertedAmount2 = $this->exchangeRate->convert($amount2, $currency2, $outputCurrency, $precision);
 
         return $convertedAmount1 + $convertedAmount2;
     }
-
-    /**
-     * @inheritDoc
-     */
+    
     public function substractAmount(
         float $minuendAmount,
         string $minuendCurrency,
@@ -45,56 +40,40 @@ final class Operations implements IOperations
         string $subtrahendCurrency,
         string $outputCurrency,
         ?int $precision
-    ): float {
-        $convertedMinuendAmount = $this->exchangeRate->convert(
-            $minuendAmount,
-            $minuendCurrency,
-            $outputCurrency,
-            $precision
-        );
-        $convertedSubtrahendAmount = $this->exchangeRate->convert(
-            $subtrahendAmount,
-            $subtrahendCurrency,
-            $outputCurrency,
-            $precision
-        );
+    ): float
+    {
+        $convertedMinuendAmount = $this->exchangeRate->convert($minuendAmount, $minuendCurrency, $outputCurrency, $precision);        
+        $convertedSubtrahendAmount = $this->exchangeRate->convert($subtrahendAmount, $subtrahendCurrency, $outputCurrency, $precision);
 
         return $convertedMinuendAmount - $convertedSubtrahendAmount;
     }
-
-    /**
-     * @inheritDoc
-     */
+    
     public function multiplyAmount(
         float $amount,
         string $currency,
         int $multiplier,
         string $outputCurrency,
         ?int $precision
-    ): float {
+    ): float
+    {
         return $this->exchangeRate->convert(($amount * $multiplier), $currency, $outputCurrency, $precision);
     }
-
-    /**
-     * @inheritDoc
-     */
+    
     public function divideAmount(
         float $amount,
         string $currency,
         int $divisor,
         string $outputCurrency,
         ?int $precision
-    ): float {
+    ): float
+    {
         if ($divisor === 0) {
             throw new \InvalidArgumentException('Divisor cannot equal 0');
         }
 
         return $this->exchangeRate->convert(($amount / $divisor), $currency, $outputCurrency, $precision);
     }
-
-    /**
-     * @inheritDoc
-     */
+    
     public function compareAmounts(
         float $amount1,
         string $currency1,
@@ -102,21 +81,16 @@ final class Operations implements IOperations
         string $currency2,
         string $outputCurrency,
         ?int $precision
-    ): float {
+    ): float
+    {
         return $this->substractAmount($amount1, $currency1, $amount2, $currency2, $outputCurrency, $precision);
     }
 
-    /**
-     * @inheritDoc
-     */
     public function exchangeAmount(float $amount, string $currencyFrom, string $currencyTo, ?int $precision): float
     {
         return $this->exchangeRate->convert($amount, $currencyFrom, $currencyTo, $precision);
     }
-
-    /**
-     * @inheritDoc
-     */
+    
     public function roundAmount(float $amount, ?int $precision): float
     {
         return $this->exchangeRate->roundAmount($amount, intval($precision));
